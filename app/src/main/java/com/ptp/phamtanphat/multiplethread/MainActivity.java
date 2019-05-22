7f02a7025077d174f127a6ad49c718ab014f428c
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-05-22 11:13:39 +0700

    a

--- app/src/main/java/com/ptp/phamtanphat/multiplethread/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/multiplethread/MainActivity.java
@@ -1,43 +1,52 @@
 package com.ptp.phamtanphat.multiplethread;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    Dataobject dataobject;
-
+    Dataobject dataobject = new Dataobject();
+    Thread threada , threadb , threadc;
+    int y,z = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dataobject.setLaco(0);
 
-        dataobject = new Dataobject();
-        dataobject.setLaco(1);
-        Thread threadA = new Thread(new Runnable() {
+//        Chỉ tạo ra 3 luồng trong hệ thống
+
+        threada = new Thread(new Runnable() {
             @Override
             public void run() {
                 synchronized (dataobject){
-                    for (int i = 0; i < 50; ) {
-                        if (dataobject.getLaco() == 1) {
-                            int a = new Random().nextInt(100);
-                            dataobject.setA(a);
-                            Log.d("BBB","Vi tri cua i " + i);
-                            Log.d("BBB", "Số a : " + dataobject.getA());
-
+                    for(int i = 0 ; i< 10 ; ){
+                        if (dataobject.getLaco() == 0){
+                            int a = new Random().nextInt(10);
+                            Log.d("BBB","A " +a + " index " + i);
+                            i++;
                             try {
-                                Thread.sleep(100);
+                                Thread.sleep(10);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
-                            i++;
-                            dataobject.setLaco(2);
+                            dataobject.setA(a);
+                            dataobject.setLaco(1);
                             dataobject.notifyAll();
-                        } else {
+                        }else{
                             try {
                                 dataobject.wait();
                             } catch (InterruptedException e) {
@@ -47,33 +56,36 @@ public class MainActivity extends AppCompatActivity {
                     }
                     dataobject.setLaco(0);
                     dataobject.notifyAll();
-                }
 
+                }
             }
         });
-        Thread threadb = new Thread(new Runnable() {
+
+
+        threadb = new Thread(new Runnable() {
             @Override
             public void run() {
                 synchronized (dataobject){
-                    for (; true;) {
-                        if (dataobject.getLaco() == 2 || dataobject.getLaco() == 0) {
-                            int b = new Random().nextInt(100);
-                            dataobject.setB(b);
-                            Log.d("BBB", "Số b : " + dataobject.getB());
-
-
+                    for(;true;){
+                        if (dataobject.getLaco() == 1 || dataobject.getLaco() == 0){
+                            int b = new Random().nextInt(10);
                             try {
-                                Thread.sleep(100);
+                                Thread.sleep(10);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
+                            Log.d("BBB","B " +b + " index " + y++);
                             if (dataobject.getLaco() == 0){
-                                dataobject.setLaco(4);
+                                dataobject.setLaco(3);
+                                dataobject.notifyAll();
                                 break;
                             }
-                            dataobject.setLaco(3);
+                            dataobject.setB(b);
+                            dataobject.setLaco(2);
                             dataobject.notifyAll();
-                        } else {
+
+
+                        }else{
                             try {
                                 dataobject.wait();
                             } catch (InterruptedException e) {
@@ -81,43 +93,52 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                     }
-                }
 
+                }
             }
         });
-        Thread threadtinhtong = new Thread(new Runnable() {
+        threadc = new Thread(new Runnable() {
             @Override
             public void run() {
-                synchronized (dataobject) {
-                    for (; true; ) {
-                        if (dataobject.getLaco() == 3 || dataobject.getLaco() == 4) {
-                            Log.d("BBB", "Tổng : " + dataobject.Tinhtong());
-
+                synchronized (dataobject){
+                    for( ; true ;){
+                        if (dataobject.getLaco() == 2 || dataobject.getLaco() == 3){
+                            int c = dataobject.getA() + dataobject.getB();
                             try {
-                                Thread.sleep(100);
+                                Thread.sleep(10);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
-                            if (dataobject.getLaco() == 4){
+                            Log.d("BBB","Gia tri c" + c + " index " + z++);
+                            if (dataobject.getLaco() ==3){
+                                dataobject.notifyAll();
                                 break;
                             }
-                            dataobject.setLaco(1);
+                            dataobject.setLaco(0);
                             dataobject.notifyAll();
-                        } else {
+
+
+                        }else{
                             try {
                                 dataobject.wait();
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
+
                         }
                     }
+
+
                 }
             }
         });
 
-        threadA.start();
+
+        threada.start();
         threadb.start();
-        threadtinhtong.start();
+        threadc.start();
+
 
     }
+
 }
